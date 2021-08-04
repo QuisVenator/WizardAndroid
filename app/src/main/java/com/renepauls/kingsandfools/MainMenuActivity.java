c2e9c0a37cbede81173648185dca2f3b6bfedb45
Manuel René Pauls Toews <paulsmrene@live.com>
2021-08-03 20:00:10 -0400

    Added basic activities for main menu and lobby without logic

---
+++ app/src/main/java/com/renepauls/kingsandfools/MainMenuActivity.java
@@ -0,0 +1,20 @@
+package com.renepauls.kingsandfools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void hostNewGame(View view) {
+
+    }
+}
